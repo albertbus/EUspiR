71a9d6b4319ae190970fd7eb9cdc2493b17b88a1
Albert Busca <albertbuscalopez@gmail.com>
2024-02-27 19:48:42 +0100

    Changed the test to reflect the new function structure.

--- tests/testthat/test-spi_indicator_sum.R
+++ tests/testthat/test-spi_indicator_sum.R
@@ -4,7 +4,7 @@ test_that("spi_indicator_sum() works", {
                      "Indicator2" = c(99, 5, 30),
                      "Indicator3" = c(90, 31, 71))
 
-  expect_equal(spi_indicator_sum(data, "Component1"),
+  expect_equal(spi_indicator_sum("regional", data, "Component1"),
                data.frame("Regions" = c("Region A", "Region B", "Region C"),
                           "Indicator1" = c(8, 61, 92),
                           "Indicator2" = c(99, 5, 30),
