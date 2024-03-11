95444d6c03196f7376bde429088f1cb148908882
Albert Busca <albertbuscalopez@gmail.com>
2024-03-11 19:39:59 +0100

    Added functionality: - Now can provide short and long results with the paramter "append" - Now works at the regional and national level - Now works for components to dimensions and for dimensions to aggregated index - Now works seamlessly with the official data

--- R/spi_component_sum.R
+++ R/spi_component_sum.R
@@ -22,7 +22,7 @@
 
 #This function aggregates components following the EU-SPI (2020) methodology
 #This function aggregates dimensions following the EU-SPI (2020) methodology
-spi_component_sum <- function(type, dataSet = NULL, dimensionName = "all", b=0.5){
+spi_component_sum <- function(type, dataSet = NULL, dimensionName = "all", b=0.5, component_index = NULL, append = TRUE){
   #Handle given data in case of potential errors
   stopifnot(is.character(type))
   if (!is.null(dataSet)) {stopifnot(is.data.frame(dataSet))}
@@ -31,30 +31,58 @@ spi_component_sum <- function(type, dataSet = NULL, dimensionName = "all", b=0.5
 
   #Retrieve data if necessary
   if (is.null(dataSet)) {
-    if (type == "weighted") {dataSet <- weighted_spi("all")}
-    else {dataSet <- spi_indicator_sum("national")}
+    if (type == "regional dimension") {dataSet <- weighted_spi("all", append = FALSE)}
+    if (type == "regional spi") {dataSet <- spi_component_sum("regional dimension", append = FALSE)}
+    if (type == "national dimension") {dataSet <- spi_indicator_sum("national", append = FALSE)}
+    if (type == "national spi") {dataSet <- spi_component_sum("national dimension", append = FALSE)}
   }
 
   #Set the dimension(s) name
-  if (dimensionName == "all") {dimensionName <- c("Basic Human Needs",
-                                                 "Foundations of Well-Being",
-                                                 "Opportunity")}
+  if (dimensionName == "all") {
+    if (type == "regional dimension" | type == "national dimension") {
+      dimensionName <- c("Basic Human Needs",
+                         "Foundations of Well-Being",
+                         "Opportunity")
+    }
+    if (type == "regional spi" | type == "national spi") {dimensionName <- "EU-SPI"}
+  }
 
-  #First step: elevate the values to b
-  elevate_to_b <- dataSet[which(sapply(dataSet, is.numeric))[1]:ncol(dataSet)]^b
+  #Set the dimension(s) index
+  if (is.null(component_index)) {
+    if (type == "regional dimension") {component_index <- list(c(4:7),c(8:11),c(12:15))}
+    if (type == "regional spi") {component_index <- list(c(4:6))}
+    if (type == "national dimension") {component_index <- list(c(3:6),c(7:10),c(11:14))}
+    if (type == "national spi") {component_index <- list(c(3:5))}
+  }
 
-  #Second step: Sum by row (region)
-  sum_row <- rowSums(elevate_to_b, na.rm = TRUE)
+  result <- dataSet[sapply(dataSet, is.character)]
 
-  #Third step: 1 divided by the number of indicators
-  one_indicator_n <- 1/(rowSums(!is.na(dataSet)) - sum(sapply(dataSet,is.character)))
+  for(n in 1:length(component_index)) {
+    #First step: Retrieve data for the aggregation
+    agg_data <- cbind(dataSet[which(sapply(dataSet, is.character))],
+                      dataSet[component_index[[n]]])
 
-  #Fourth step: sum_row * one_indicator_n
-  fourth_step <- sum_row*one_indicator_n
+    #Second step: elevate the values to b
+    elevate_to_b <- agg_data[which(sapply(agg_data, is.numeric))[1]:ncol(agg_data)]^b
 
-  #Fifth step: elevate the values to (1/b)
-  dataSet[dimensionName] <- fourth_step^(1/b)
+    #Third step: Sum by row (region)
+    sum_row <- rowSums(elevate_to_b, na.rm = TRUE)
+
+    #Fourth step: 1 divided by the number of indicators
+    one_indicator_n <- 1/(rowSums(!is.na(agg_data)) - sum(sapply(agg_data, is.character)))
+
+    #Fifth step: sum_row * one_indicator_n
+    fifth_step <- sum_row*one_indicator_n
+
+    #Sixth step: elevate the values to (1/b)
+    agg_data[dimensionName[n]] <- fifth_step^(1/b)
+
+    #Return the dimension(s)
+    agg_data <- agg_data[sapply(agg_data, is.numeric)]
+    if(append == TRUE){result <- cbind(result, agg_data)}
+    else{result <- cbind(result, agg_data[ncol(agg_data)])}
+  }
 
   #Retrieve the aggregated dimension
-  return(dataSet)
+  return(result)
 }
