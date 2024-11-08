18021ecf82f15b10cc6ab684600043b76a64d3c0
albertbus <159279358+albertbus@users.noreply.github.com>
2024-11-08 21:57:28 +0100

    Update spi_normalisation.R
    Updated the documentation of the function. Added error handling to ease user experience and facilitate the correct use of the function.

--- R/spi_normalisation.R
+++ R/spi_normalisation.R
@@ -1,7 +1,7 @@
 #' Normalisation function within EU-SPI (2020)
 #' @description
-#' Normalise the data accordin to the methodology seen in the EU-SPI(2020). It
-#' only works with single values, not matrices of data.
+#' Normalise the data according to the methodology seen in the EU-SPI (2020). It
+#' only works with single values for vectors or matrices, see \link{spi_normalisation_m}.
 #'
 #' @param val Value that needs to be normalised
 #' @param dis_val Dystopic value of the indicator
@@ -20,6 +20,13 @@
 
 #This function normalises a value following the EU-SPI (2020) methodology
 spi_normalisation <- function(val,dis_val,uto_val,inverted){
+  # Error handling
+  if(!is.numeric(val)) { stop(paste0("Error: 'val' must be numeric, but it was of type: ", typeof(val), ". Please provide a number."))}
+  if(!is.numeric(dis_val)) { stop(paste0("Error: 'dis_val' must be numeric, but it was of type: ", typeof(dis_val), ". Please provide a number."))}
+  if(!is.numeric(uto_val)) { stop(paste0("Error: 'uto_val' must be numeric, but it was of type: ", typeof(uto_val), ". Please provide a number."))}
+  if(!is.logical(inverted)) { stop(paste0("Error: 'inverted' must be logical, but it was of type: ", typeof(inverted), "."))}
+
+  # Normalisation
   if(inverted==TRUE) return(((-100*(val-uto_val))/(dis_val-uto_val))+100)
   if(inverted==FALSE) return((100*(val-dis_val))/(uto_val-dis_val))
 }
